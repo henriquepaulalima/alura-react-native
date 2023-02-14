22c9b0e78600e943940dc853694dbc1d78aa6588
henriquepaulalima <henrique.lima@fluna.io>
2023-02-14 19:48:01 -0300

    Add items to basket mock

--- orgs-cesta/src/mocks/basket.js
+++ orgs-cesta/src/mocks/basket.js
@@ -1,4 +1,9 @@
 import farmImg from '../../assets/logo.png';
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import pepino from '../../assets/frutas/Pepino.png';
+import abobora from '../../assets/frutas/Abóbora.png';
 
 const basket = {
   top: {
@@ -10,6 +15,31 @@ const basket = {
     farmName: 'Jenny Jack Farm',
     description: 'Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha',
     price: 'R$ 40,00',
+  },
+  items: {
+    title: "Itens da cesta",
+    list: [
+      {
+        name: "Tomate",
+        image: tomate,
+      },
+      {
+        name: "Brócolis",
+        image: brocolis,
+      },
+      {
+        name: "Batata",
+        image: batata,
+      },
+      {
+        name: "Pepino",
+        image: pepino,
+      },
+      {
+        name: "Abóbora",
+        image: abobora,
+      }
+    ]
   }
 }
 
