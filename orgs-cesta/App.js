6fb2a8272d8e7b7ac7010729e6525e95c659a0af
henriquepaulalima <henrique.lima@fluna.io>
2023-02-23 19:14:39 -0300

    Replace map with FlatList

--- orgs-cesta/App.js
+++ orgs-cesta/App.js
@@ -15,7 +15,7 @@ export default function App() {
   }
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar />
       <Baskets {...mock} />
     </SafeAreaView>
--- orgs-cesta/src/views/Baskets/index.js
+++ orgs-cesta/src/views/Baskets/index.js
@@ -1,22 +1,42 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import Top from "./Top";
 import Details from "./Details";
-import Items from "../../components/Items";
+import Item from "../../components/Item";
+import Text from "../../components/Text";
 
 export default function Baskets({ top, details, items }) {
   return (
-    <ScrollView>
-      <Top {...top} />
-      <View style={styles.basket}>
-        <Details {...details} />
-        <Items {...items} />
-      </View>
-    </ScrollView>
+    <>
+      <FlatList
+        data={items.list}
+        renderItem={Item}
+        keyExtractor={({ name }) => name}
+        ListHeaderComponent={() => {
+          return (
+            <>
+              <Top {...top} />
+              <View style={styles.basket}>
+                <Details {...details} />
+                <Text style={styles.title}>{items.title}</Text>
+              </View>
+            </>
+          );
+        }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    color: '#464646',
+    fontWeight: 'bold',
+    marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
+  },
   basket: {
     paddingVertical: 8,
     paddingHorizontal: 16,
