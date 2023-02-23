e3a56f16a8f23a61f31b8a1f2003390dbfd35cdd
henriquepaulalima <henrique.lima@fluna.io>
2023-02-23 18:50:16 -0300

    Create Items component

--- orgs-cesta/src/views/Baskets/index.js
+++ orgs-cesta/src/views/Baskets/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import Top from "./Top";
 import Details from "./Details";
+import Items from "../../components/Items";
 
-export default function Baskets({ top, details }) {
+export default function Baskets({ top, details, items }) {
   return (
-    <>
+    <ScrollView>
       <Top {...top} />
       <View style={styles.basket}>
         <Details {...details} />
+        <Items {...items} />
       </View>
-    </>
+    </ScrollView>
   );
 }
 
